d8d2f602efa007d1162719dc2527ae600c1a0225
VniciDigital <91447833+GaaraSan01@users.noreply.github.com>
2022-07-31 02:03:00 -0300

    Up

---
+++ Entendendo metodo POST/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.listen('3000')
+
+//middleware
+app.use(express.json())
+app.route('/').post( (req,res) => res.send(req.body))
---
+++ Metodo PUT/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+app.listen('3000')
+app.use(express.json())
+
+let author = "Vinicius Henrique"
+app.route('/').get((req,res) => res.send(author))
+app.route('/').put( (req,res) => {
+    author = req.body.author
+    res.send(author)
+})
